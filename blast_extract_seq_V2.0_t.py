f81afaea14e85568a88be160934b6db5c83aa7f0
Xiao Fei <china-fixing@hotmail.com>
2024-05-06 13:18:32 +0000

    test pass

--- blast_extract_seq_V2.0_t.py
+++ blast_extract_seq_V2.0_t.py
@@ -9,6 +9,7 @@ This robot can help to locally blast the query to the reference genomes and extr
 20221031---upgrade V1.3 to add function to caculate the extracted seq types distribution
 
 *********20240501---major upgrade, add more blast functions, and csv summary file for further data analaysis***********
+20240501---update to add multiprocessing to hugely improve the running speed!
 '''
 
 
@@ -23,6 +24,34 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 import pandas as pd
+import logging
+
+
+def setup_logger(log_file):
+    # Create logger
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    # Create console handler and set level to INFO
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+
+    # Create file handler and set level to INFO
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+
+    # Create formatter
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    # Add formatter to console handler and file handler
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    # Add console handler and file handler to logger
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    return logger
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Xiao_Fei_Robot: Local BLAST and Sequence Analysis Tool")
@@ -53,39 +82,6 @@ def perform_blast(query, reference, blast_type, num_threads):
         sys.exit(1)
     blast_xml = io.BytesIO(blast_result.stdout)
     return blast_xml
-
-# def filter_matches(cutoff, blast_xml, reference_name, coverage_cutoff):
-#     matched_seqs = []
-#     blast_records = NCBIXML.parse(blast_xml)
-#     for blast_record in blast_records:
-#         query_length =blast_record.query_letters
-#         query = blast_record.query
-#         for alignment in blast_record.alignments:
-#             hsp = alignment.hsps[0]
-#             alignment_coverage = hsp.align_length / query_length
-#             if (hsp.identities / query_length >= cutoff) and (alignment_coverage >= coverage_cutoff) and hsp.expect <= 1e-4:
-#                 # Store only the query sequence in the matched_seq object
-#                 matched_seq = (blast_record.query, SeqRecord(Seq(hsp.sbjct), id=alignment.hit_def, description=(reference_name+" "+query)))
-#                 matched_seqs.append(matched_seq)
-#     return matched_seqs
-
-# def filter_matches_protein(cutoff, blast_xml, reference_name, coverage_cutoff,reference_file):
-#     matched_seqs = []
-#     blast_records = NCBIXML.parse(blast_xml)
-#     for blast_record in blast_records:
-#         query_length =blast_record.query_letters
-#         query = blast_record.query
-#         for alignment in blast_record.alignments:
-#             hsp = alignment.hsps[0]
-#             alignment_coverage = hsp.align_length / query_length
-#             if (hsp.identities / query_length >= cutoff) and (alignment_coverage >= coverage_cutoff) and hsp.expect <= 1e-4:
-#                 with open(reference_file, 'r') as ref_file:
-#                     for seq_record in SeqIO.parse(ref_file, 'fasta'):
-#                         if seq_record.id == alignment.hit_id:
-#                             matched_seq = (blast_record.query, SeqRecord(seq_record.seq, id=alignment.hit_def, description=(reference_name+" "+query)))
-#                             matched_seqs.append(matched_seq)
-#                             break     
-#     return matched_seqs
 
 def filter_matches_combined(cutoff, blast_xml, reference_name, coverage_cutoff, is_protein=False, reference_file=None):
     matched_seqs = []
@@ -128,21 +124,20 @@ def convert_to_original_format(modified_matched_seqs):
     
     return original_matched_seqs
 
-# def blast_and_extract(query_file, reference_folder, cutoff, blast_type, output_file, coverage_cutoff):
+
+# def blast_and_extract(query_file, reference_folder, cutoff, blast_type, output_file, coverage_cutoff,num_threads):
 #     reference_files = glob.glob(os.path.join(reference_folder, '*'))
 #     matched_seqs = []
 
 #     for reference_file in reference_files:
 #         reference_name = os.path.basename(reference_file)
-#         blast_xml = perform_blast(query_file, reference_file, blast_type)
-#         if blast_type == 'blastp':
-#             matched_seqs.extend(filter_matches_protein(cutoff, blast_xml, reference_name,coverage_cutoff,reference_file))
-#         else:
-#             matched_seqs.extend(filter_matches(cutoff, blast_xml, reference_name, coverage_cutoff))
-#     matched_seqs_seq=convert_to_original_format(matched_seqs)
+#         blast_xml = perform_blast(query_file, reference_file, blast_type,num_threads)
+#         is_protein = (blast_type == 'blastp')
+#         matched_seqs.extend(filter_matches_combined(cutoff, blast_xml, reference_name, coverage_cutoff, is_protein, reference_file))
+        
+#     matched_seqs_seq = convert_to_original_format(matched_seqs)
 #     SeqIO.write(matched_seqs_seq, output_file, "fasta")
 #     return matched_seqs
-
 import multiprocessing
 from functools import partial  # Add this import statement
 def process_reference(reference_file, query_file, cutoff, blast_type, coverage_cutoff, num_threads, matched_seqs):
@@ -168,6 +163,7 @@ def blast_and_extract(query_file, reference_folder, cutoff, blast_type, output_f
     matched_seqs_seq = convert_to_original_format(matched_seqs)
     SeqIO.write(matched_seqs_seq, output_file, "fasta")
     return matched_seqs
+
 
 def is_pseudo_sequence(aligned_sequence):
     """Check if the aligned sequence is a pseudo-sequence."""
@@ -196,16 +192,8 @@ def is_pseudo_sequence(aligned_sequence):
 
     return pseudo_status
 
-
-def generate_summary(output_file, blast_type):
-    # summary_file = output_file + "_summary"
-    # with open(summary_file, "w") as f:
-    #     for query, seq in matched_seqs:
-    #         if blast_type == 'blastp':
-    #             match_status = is_pseudo_sequence(seq.seq)
-    #         else:
-    #             match_status = "match" if len(seq) > 0 else "unmatch"
-    #         print(query + "\t" + seq.description + "\t" +seq.id+"\t"+ match_status, file=f)
+from itertools import product
+def generate_summary(output_file, blast_type, reference_folder,query_file):
     try:
         subprocess.run(["seqkit", "version"], check=True, stdout=subprocess.PIPE)
     except FileNotFoundError:
@@ -216,49 +204,76 @@ def generate_summary(output_file, blast_type):
     cmd = 'seqkit fx2tab ' + output_file + ' > ' + output_file + '.tab'
     os.system(cmd)
 
+    try:
+        subprocess.run(["seqkit", "version"], check=True, stdout=subprocess.PIPE)
+    except FileNotFoundError:
+        print("Error: seqkit is not installed or not found in the system path.")
+        print("Please install seqkit to perform sequence analysis.")
+        sys.exit(1)
+
+    cmd = 'seqkit fx2tab ' + query_file + ' > ' + output_file + '_query_file.tab'
+    os.system(cmd)
+
     tab_df = pd.read_csv(output_file + '.tab', sep='\t', names=['assembly_name', 'seq'], index_col=False)
     split_names = tab_df['assembly_name'].str.rsplit(n=4, expand=True)
     tab_df[['subject_id', 'subject_file', 'query_id','alignment_coverage','alignment_identity']] = split_names
     if blast_type == 'blastp' or blast_type == 'tblastn':
         tab_df['is_pseudo'] = tab_df['seq'].apply(is_pseudo_sequence)
-    tab_df.to_csv(output_file + "_SUM" + ".csv")
-    return tab_df
 
-# def analyze_sequence_types(output_file):
-#     try:
-#         subprocess.run(["seqkit", "version"], check=True, stdout=subprocess.PIPE)
-#     except FileNotFoundError:
-#         print("Error: seqkit is not installed or not found in the system path.")
-#         print("Please install seqkit to perform sequence analysis.")
-#         sys.exit(1)
+    query_id_list=pd.read_csv(output_file + '_query_file.tab', sep='\t',names=['query_id','query_seq'],index_col=False)['query_id']
+    subject_file_list_raw = glob.glob(os.path.join(reference_folder, '*'))
+    subject_file_list = [os.path.basename(file_path) for file_path in subject_file_list_raw]
+    # filter_list=tab_df['subject_file'].unique()
+    link=list(product(query_id_list,subject_file_list))
+    df_link =  pd.DataFrame(link, columns=['query_id','subject_file'])
+    # print(df_link)
+    tab_df_merge=pd.merge(df_link, tab_df, left_on=['query_id','subject_file'], right_on=['query_id','subject_file'], how='left')
+    # print(tab_df_merge)
+    tab_df_merge['is_pseudo'] = tab_df_merge['is_pseudo'].fillna('Absent')
+    tab_df_merge[['alignment_coverage','alignment_identity']] = tab_df_merge[['alignment_coverage','alignment_identity']].fillna(0)
+    tab_df_merge['seq'].fillna('NO_MATCH', inplace=True)
 
-#     cmd = 'seqkit fx2tab ' + output_file + ' > ' + output_file + '.tab'
-#     os.system(cmd)
+    tab_df_merge.to_csv(output_file + "_SUM" + ".csv")
+    tab_df_merge.to_pickle(output_file + "_SUM" + ".pickle")
+    return tab_df_merge
 
-#     tab_df = pd.read_csv(output_file + '.tab', sep='\t', names=['assembly_name', 'seq'], index_col=False)
-#     split_names = tab_df['assembly_name'].str.rsplit(n=2, expand=True)
-#     tab_df[['subject_id', 'subjuect_file', 'query_id']] = split_names
-#     tab_df
-#     tab_df['is_pseudo'] = tab_df['seq'].apply(is_pseudo_sequence)
-#     # df = tab_df.groupby('seq').count().sort_values(by='assembly_name', ascending=0)
-#     # df.reset_index(inplace=True)
-#     # df.rename(columns={'assembly_name': 'count'}, inplace=True)
-#     # df['frequency'] = df['count'] / df['count'].sum()
-#     return tab_df
-
-# def main():
-#     args = parse_arguments()
-#     blast_and_extract(args.query, args.reference_folder, args.cutoff_identity,
-#                                       args.blast_type, args.output, args.cutoff_coverage, args.num_threads)
-#     generate_summary(args.output, args.blast_type)
-#     print('Xiao_Fei_Robot: Job done!')
 
 
 ### quick_see module is use to analysis the _sum.csv, summarized the matched gene distribution
-def quick_see(input, output_file):
+def quick_see(input, output_file, query_file):
     df_one_match_each_queryfile = input.groupby(['query_id', 'subject_file'], as_index=False).apply(lambda x: x.sort_values('alignment_identity', ascending=False).iloc[0])
     result = df_one_match_each_queryfile.groupby(['query_id','seq'])['is_pseudo'].apply(lambda x: x.value_counts()).reset_index(name='count')
+    
+    
+    # Add a new column 'mutation' with default value 'pseudo'
+    result['mutation'] = 'Pseudo'
+
+    # Update 'mutation' column where 'level_2' is 'Absent' or 'Intact'
+    result.loc[result['level_2'] == 'Absent', 'mutation'] = 'Absent'
+    result.loc[result['level_2'] == 'Intact', 'mutation'] = 'Intact'
+    
+    result['total_subject_count'] = result.groupby(['query_id'])['count'].transform('sum')
+    result['percentage'] = (result['count'] / result['total_subject_count'])
+    # try:
+    #     subprocess.run(["seqkit", "version"], check=True, stdout=subprocess.PIPE)
+    # except FileNotFoundError:
+    #     print("Error: seqkit is not installed or not found in the system path.")
+    #     print("Please install seqkit to perform sequence analysis.")
+    #     sys.exit(1)
+
+    # cmd = 'seqkit fx2tab ' + query_file + ' > ' + output_file + '_query_file.tab'
+    # os.system(cmd)
+    # query_df = pd.read_csv(output_file + '_query_file.tab', sep='\t',names=['query_id','query_seq'],index_col=False)
+    # result_merge = query_df.merge(result, on='query_id', how='outer')
+    # result_merge['level_2'].fillna('absent', inplace=True)
     result.to_csv(output_file+"_quicksee.csv")
+    result.to_pickle(output_file+"_quicksee.pickle")
+
+    T=result.groupby(['query_id','level_2'])[['count','percentage']].sum().reset_index()
+    T.to_csv(output_file+"_quicksee_1.csv")
+    K=result.groupby(['query_id','mutation'])[['count','percentage']].sum().reset_index()
+    K.to_csv(output_file+"_quicksee_2.csv")
+
     return result
 
 
@@ -278,15 +293,12 @@ def main():
     # Create a log file path
     log_file = os.path.join(output_folder, "logfile.txt")
     
-    # Print the command used to both terminal and log file
+    # Setup logger to output to both console and file
+    logger = setup_logger(log_file)
+
+   # Log the command used
     command_used = " ".join(sys.argv)
-    # print("Command used:", command_used)
-    with open(log_file, 'a') as f:
-        print("Command used:", command_used, file=f)
-    
-    # Redirect stdout and stderr to the log file
-    # sys.stdout = open(log_file, 'a')
-    # sys.stderr = open(log_file, 'a')
+    logger.info("Command used: %s", command_used)
 
 
     # Perform BLAST search and analysis
@@ -294,7 +306,7 @@ def main():
                       args.blast_type, output_file, args.cutoff_coverage, args.num_threads)
     
     # Generate summary and save it to the output folder
-    tab_df = generate_summary(output_file, args.blast_type)
+    tab_df = generate_summary(output_file, args.blast_type, args.reference_folder,args.query)
     
     print('Xiao_Fei_Robot: Job done!')
 
@@ -302,7 +314,7 @@ def main():
         print("hey xiao, i am doing advance module now!")
         if args.xiao_dev == 'quick_see':
             print("run quick_see module now!")
-            quick_see(tab_df, output_file)
+            quick_see(tab_df, output_file, query_file=args.query)
         else:
             print("hey xiao, no match to you advance function!")
     print('THE END')
